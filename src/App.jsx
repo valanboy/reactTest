c961bed2df1b40ba12181694e1dcdb167d4191fd
Believe Gilbert <believegilbert20@gmail.com>
2025-05-13 10:59:09 +0100

    added tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import './App.css'
 import NavBar from './components/NavBar'
-import Skills from './components/Skills'
 import Home from './components/Home'
 
 function App() {
@@ -9,7 +8,7 @@ function App() {
     <div>
     <NavBar/>
     <Home/>
-    <Skills/>
+  
     </div>
   )
 }
